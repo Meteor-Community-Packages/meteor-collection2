00bf5f2535b059e89ad35b991f94391b46632d79
Eric Dobbertin <eric@dairystatedesigns.com>
2013-07-22 16:16:16 -0500

    fix load order

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-  summary: "Wraps Meteor.Collection to provide support for automatic validation of insert and update operations on the client and server, plus simple virtual field support."
+    summary: "Wraps Meteor.Collection to provide support for automatic validation of insert and update operations on the client and server, plus simple virtual field support."
 });
 
 Package.on_use(function(api) {
-  api.use(['underscore']);
-  api.add_files(['collection2.js'], ['client', 'server']);
+    api.use('underscore', ['client', 'server']);
+    api.use('deps', 'client');
+    api.use('startup', 'client');
+    api.use('simple-schema', ['client', 'server']);
+
+    api.add_files(['collection2.js'], ['client', 'server']);
 });
