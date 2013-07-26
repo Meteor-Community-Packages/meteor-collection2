660b9972ffb4c6c8cac71211118d1165971a95fc
Eric Dobbertin <eric@dairystatedesigns.com>
2013-07-26 03:58:58 -0500

    move reactivity to simpleschema

--- collection2.js
+++ collection2.js
@@ -1,16 +1,6 @@
 Meteor.Collection2 = function(name, options) {
     var self = this, userTransform = options.transform;
     options = options || {};
-    //public properties
-    self.invalidFields = [];
-    self.deps = {};
-    //set up validation dependencies
-    var fieldNames = _.keys(options.schema);
-    _.each(fieldNames, function(name) {
-        self.deps[name] = new Deps.Dependency;
-    });
-    //private properties
-    self._schema = options.schema;
     self._name = name;
     self._simpleSchema = new SimpleSchema(options.schema);
     if ("schema" in options) {
@@ -44,7 +34,6 @@ _.extend(Meteor.Collection2.prototype, {
         var self = this,
                 collection = self._collection,
                 schema = self._simpleSchema,
-                addedFields = [], removedFields = [], changedFields = [],
                 doc, callback, error;
 
         if (!args.length) {
@@ -81,31 +70,13 @@ _.extend(Meteor.Collection2.prototype, {
             };
         }
 
-        //whitelist
+        //clean up doc
         doc = schema.filter(doc);
         doc = schema.autoTypeConvert(doc);
-        
-        //note any currently invalid fields so that we can mark them as changed
-        //due to new validation (they may be valid now, or invalid in a different way)
-        _.each(self.invalidFields, function(obj) {
-            removedFields.push(obj.name);
-        });
+        //validate doc
+        schema.validate(doc);
 
-        //perform validation
-        self.invalidFields = schema.validate(doc);
-
-        //add newly invalid fields to changedFields
-        _.each(self.invalidFields, function(obj) {
-            addedFields.push(obj.name);
-        });
-
-        //mark all changed fields as changed
-        changedFields = _.union(addedFields, removedFields);
-        _.each(changedFields, function(name) {
-            self.deps[name].changed();
-        });
-
-        if (!self.invalidFields.length) {
+        if (schema.valid()) {
             if (type === "insert") {
                 args[0] = doc; //update to reflect whitelist and typeconvert changes
                 return collection.insert.apply(collection, args);
@@ -142,21 +113,7 @@ _.extend(Meteor.Collection2.prototype, {
         var self = this, collection = self._collection;
         return collection.deny.apply(collection, arguments);
     },
-    resetValidation: function() {
-        var self = this, removedFields = [];
-        _.each(self.invalidFields, function(obj) {
-            removedFields.push(obj.name);
-        });
-        self.invalidFields = [];
-        _.each(removedFields, function(name) {
-            self.deps[name].changed();
-        });
-    },
-    schema: function (field) {
-        if (field) {
-            return this._schema[field];
-        } else {
-            return this._schema;
-        }
+    simpleSchema: function() {
+        return this._simpleSchema;
     }
 });
