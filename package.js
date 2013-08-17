a8066879159e111eff0c58d62aaa4d54b8174d15
Eric Dobbertin <eric@dairystatedesigns.com>
2013-08-17 12:50:58 -0500

    package updates for Meteor 0.6.5

--- package.js
+++ package.js
@@ -3,10 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function(api) {
+    api.imply('simple-schema', ['client', 'server']);
     api.use('underscore', ['client', 'server']);
     api.use('deps', 'client');
     api.use('startup', 'client');
-    api.use('simple-schema', ['client', 'server']);
-
     api.add_files(['collection2.js'], ['client', 'server']);
 });
