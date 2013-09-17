3e00717b0fcc61b3bbb84162146c1d3d764cdef1
Morten Henriksen <mh@gi2.dk>
2013-09-17 18:39:07 +0200

    Added initial tests
    make an account at https://travis-ci.org/
    enable the collection2 repo
    add the build image to the readme (right click on the build image in
    travis to get the md - takes awhile until tests are run - you'll get an
    email)
    
    This is just initial tests - guess all operations should be tested -
    for now insert is added, but update would be next

--- package.js
+++ package.js
@@ -4,11 +4,19 @@ Package.describe({
 
 Package.on_use(function(api) {
     api.use('simple-schema', ['client', 'server']);
-    if (typeof api.imply !== "undefined") {
-        api.imply('simple-schema', ['client', 'server']);
-    }
+    api.imply && api.imply('simple-schema', ['client', 'server']);
+
     //api.use('smart-collections', ['client', 'server'], {weak: true}); //weak dependencies are broken; removing this until fixed (meteor/meteor#1358)
     api.use('underscore', ['client', 'server']);
     api.use('deps', ['client', 'server']);
     api.add_files(['collection2.js'], ['client', 'server']);
+});
+
+Package.on_test(function (api) {
+  api.use('collection2');
+  api.use('test-helpers', 'server');
+  api.use(['tinytest', 'underscore', 'ejson', 'ordered-dict',
+           'random', 'deps']);
+
+  api.add_files('collection2.tests.js', ['client', 'server']);
 });
