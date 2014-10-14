4e9be2e28b8102b37690b16f0d68406a049f9f55
Eric Dobbertin <eric@dairystatedesigns.com>
2014-10-14 12:29:58 -0500

    package updates

--- package.js
+++ package.js
@@ -7,44 +7,32 @@ Package.describe({
 
 Package.on_use(function(api) {
 
-  if (api.versionsFrom) {
-    api.use(['aldeed:simple-schema@1.0.3']);
-    api.imply(['aldeed:simple-schema']);
+  api.use(['aldeed:simple-schema@1.0.3']);
+  api.imply(['aldeed:simple-schema']);
 
-    api.use('underscore@1.0.0');
-    api.use('deps@1.0.0');
-    api.use('check@1.0.0');
-    api.use('mongo-livedata@1.0.0');
-    api.use('ejson@1.0.0');
+  api.use('underscore@1.0.0');
+  api.use('deps@1.0.0');
+  api.use('check@1.0.0');
+  api.use('mongo-livedata@1.0.0');
+  api.imply('mongo-livedata');
+  api.use('ejson@1.0.0');
 
-    // Allow us to detect 'insecure'.
-    api.use('insecure@1.0.0', {weak: true});
-  } else {
-    api.use(['simple-schema']);
-    api.imply(['simple-schema']);
-    api.use(['underscore', 'deps', 'check', 'mongo-livedata', 'ejson']);
-
-    // Allow us to detect 'insecure'.
-    api.use('insecure', {weak: true});
-  }
+  // Allow us to detect 'insecure'.
+  api.use('insecure@1.0.0', {weak: true});
   
   api.add_files(['collection2.js']);
 });
 
 Package.on_test(function(api) {
 
-  if (api.versionsFrom) {
-    api.use('aldeed:collection2');
-    api.use('tinytest@1.0.0');
-    api.use('test-helpers@1.0.0');
-    api.use('underscore@1.0.0');
-    api.use('ejson@1.0.0');
-    api.use('ordered-dict@1.0.0');
-    api.use('random@1.0.0');
-    api.use('deps@1.0.0');
-  } else {
-    api.use(['collection2', 'tinytest', 'test-helpers', 'underscore', 'ejson', 'ordered-dict', 'random', 'deps']);
-  }
+  api.use('aldeed:collection2');
+  api.use('tinytest@1.0.0');
+  api.use('test-helpers@1.0.0');
+  api.use('underscore@1.0.0');
+  api.use('ejson@1.0.0');
+  api.use('ordered-dict@1.0.0');
+  api.use('random@1.0.0');
+  api.use('deps@1.0.0');
 
   api.add_files([
     'tests/schemas.js',
