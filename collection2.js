1d46f4d6fee89278e3b92be0a90edafc5301522e
mokolodi1 <mokolodi1@gmail.com>
2015-11-10 17:42:57 -0800

    workaround for  specifying required value in upsert

--- collection2.js
+++ collection2.js
@@ -346,6 +346,15 @@ function doValidate(type, args, getAutoValues, userId, isFromTrustedCode) {
     var set = docToValidate.$set || {};
     docToValidate.$set = _.clone(selector);
     _.extend(docToValidate.$set, set);
+
+    var addToSet = docToValidate.$addToSet;
+    if (addToSet) {
+      var addToSetArrays = {};
+      for (var index in addToSet) {
+        addToSetArrays[index] = [addToSet[index]];
+      }
+      _.extend(docToValidate.$set, addToSetArrays);
+    }
   }
 
   // Set automatic values for validation on the client.
