0a9b0f917812f1446330c1b794d08a1abd835d12
Jan Dvorak <storyteller@freedombase.net>
2021-04-21 17:26:06 +0200

    Modernize & remove clone

--- package/collection2/package.js
+++ package/collection2/package.js
@@ -3,13 +3,12 @@
 Package.describe({
   name: "aldeed:collection2",
   summary: "Automatic validation of Meteor Mongo insert and update operations on the client and server",
-  version: "3.2.2",
+  version: "3.3.0",
   documentation: "../../README.md",
   git: "https://github.com/aldeed/meteor-collection2.git"
 });
 
 Npm.depends({
-  clone: '2.1.2',
   'lodash.isempty': '4.4.0',
   'lodash.isequal': '4.5.0',
   'lodash.isobject': '3.0.2',
@@ -18,10 +17,10 @@ Npm.depends({
 Package.onUse(function(api) {
   api.use('mongo@1.10.0');
   api.imply('mongo');
-  api.use('minimongo@1.6.0');
+  api.use('minimongo@1.6.2');
   api.use('ejson@1.1.1');
   api.use('raix:eventemitter@0.1.3 || 1.0.0');
-  api.use('ecmascript@0.14.3');
+  api.use('ecmascript@0.15.1');
   api.use('tmeasday:check-npm-versions@0.3.2 || 1.0.1');
 
   // Allow us to detect 'insecure'.
