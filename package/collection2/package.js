457d748a5da6185e7615dc60c2a661c84efe276c
Jan Dvorak <storytellercz@proton.me>
2024-07-16 11:52:09 +0200

    Update versions to Meteor 3.0 release

--- package/collection2/package.js
+++ package/collection2/package.js
@@ -4,7 +4,7 @@ Package.describe({
   name: 'aldeed:collection2',
   summary:
     'Automatic validation of Meteor Mongo insert and update operations on the client and server',
-  version: '4.0.2',
+  version: '4.0.3',
   documentation: '../../README.md',
   git: 'https://github.com/aldeed/meteor-collection2.git'
 });
@@ -16,14 +16,14 @@ Npm.depends({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom(['1.12.1', '2.3', '3.0-rc.4']);
+  api.versionsFrom(['1.12.1', '2.3', '3.0']);
   api.use('mongo');
   api.imply('mongo');
   api.use('minimongo');
   api.use('ejson');
   api.use('ecmascript');
   api.use('raix:eventemitter@1.0.0');
-  api.use('aldeed:simple-schema@1.13.1 || 2.0.0-beta300.0');
+  api.use('aldeed:simple-schema@1.13.1 || 2.0.0-rc.300.10');
 
   api.addFiles(['./collection2.js']);
 
@@ -34,7 +34,7 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function (api) {
-  api.versionsFrom(['1.12.1', '2.3', '3.0-rc.4']);
+  api.versionsFrom(['1.12.1', '2.3', '3.0']);
   api.use([
     'meteortesting:mocha@3.1.0-rc.1',
     'aldeed:collection2@4.0.2'
